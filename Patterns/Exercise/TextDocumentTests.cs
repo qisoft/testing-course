6efcd04494b0c0b2cfb1047b8319d66614b3045f
QiSoft <qisoft3@gmail.com>
2015-07-23 21:34:20 +0500

    bowling

--- Patterns/Exercise/TextDocumentTests.cs
+++ Patterns/Exercise/TextDocumentTests.cs
@@ -6,31 +6,51 @@ namespace Kontur.Courses.Testing.Patterns.Exercise
 	[TestFixture]
 	public class TextDocumentTests
 	{
-		[Test]
-		public void Test()
-		{
-			var doc = new TextDocument("hello world");
-			var cmd = doc.MakeReplaceCommand(0, 1, "H");
-			cmd.Do();
-			Assert.AreEqual("Hello world", doc.Text);
-			var cmd2 = doc.MakeReplaceCommand(6, 5, "all!");
-			cmd2.Do();
-			Assert.AreEqual("Hello all!", doc.Text);
-			Assert.AreEqual(2, doc.EditsCount);
-			cmd2.Undo();
-			Assert.AreEqual(1, doc.EditsCount);
-			cmd.Undo();
-			Assert.AreEqual(0, doc.EditsCount);
-			Assert.AreEqual("hello world", doc.Text);
-			try
-			{
-				cmd2.Undo();
-				Assert.Fail("Should not be able to undo command which was not executed before");
-			}
-			catch (InvalidOperationException)
-			{
-				Console.WriteLine("Exception was thrown!");
-			}
-		}
+        [TestFixture]
+        public class Replace_should
+        {
+            [TestCase("hello world", 0, 1, "H", Result = "Hello world", TestName = "1 letter at the beginning")]
+            [TestCase("Hello world", 6, 5, "all!", Result = "Hello all!", TestName = "5 letters in the end")]
+            [TestCase("world", 0, 0, "Hello ", Result = "Hello world", TestName = "nothing and insert text at the beginning")]
+            [TestCase("Helloworld", 5, 0, " ", Result = "Hello world", TestName = "nothing and insert text in the middle")]
+            public string Change(string input, int index, int count, string replacement)
+            {
+                var doc = new TextDocument(input);
+                doc.MakeReplaceCommand(index, count, replacement).Do();
+                return doc.Text;
+            }
+
+            [Test]
+            public void IncreementEditsCount()
+            {
+                var doc = new TextDocument("hello world");
+                doc.MakeReplaceCommand(0, 1, "H").Do();
+                doc.MakeReplaceCommand(0, 1, "H").Do();
+                Assert.AreEqual(2, doc.EditsCount);
+            }
+        }
+	    [TestFixture]
+	    public class Undo_should
+	    {
+	        [TestCase("hello world", TestName = "on non-empty string")]
+            [TestCase("", TestName = "on empty string")]
+	        public void RestoreDocumentToPreviousState(string input)
+	        {
+	            var doc = new TextDocument(input);
+	            var cmd = doc.MakeReplaceCommand(0, 1, "H");
+	            cmd.Do();
+	            cmd.Undo();
+	            Assert.AreEqual(0, doc.EditsCount);
+	            Assert.AreEqual(input, doc.Text);
+	        }
+
+            [Test]
+            public void WhenCommandIsNotDone_Fail()
+            {
+                var doc = new TextDocument("hello world!");
+                var cmd = doc.MakeReplaceCommand(0, 1, "H");
+                Assert.Throws<InvalidOperationException>(() => cmd.Undo());
+            }
+        }
 	}
 }
--- Tdd/BowlingGame.cs
+++ Tdd/BowlingGame.cs
@@ -5,16 +5,73 @@ using NUnit.Framework;
 
 namespace Kontur.Courses.Testing.Tdd
 {
+    public class Frame
+    {
+        private List<int> rolls = new List<int>();
+
+        public bool IsStrike => rolls[0] == 10;
+
+        public bool IsSpare => rolls.Count > 1 && rolls.Sum() == 10;
+
+        public bool IsFinished => rolls.Count == 2 || IsStrike;
+
+        private int bonus = 0;
+
+        public void AddBonus(int bonus)
+        {
+            this.bonus += bonus;
+        }
+
+        public int Score => rolls.Sum() + bonus;
+
+        public void Roll(int pins)
+        {
+            rolls.Add(pins);
+        }
+
+        public int NeedBonuses { get; set; }
+    }
+
+    
 	public class Game
 	{
+	    private readonly List<Frame> frames = new List<Frame>();
+	    private int score = 0;
 		public void Roll(int pins)
 		{
-			
+            CheckSpareOrStrike();
+            var currentFrame = frames.LastOrDefault();
+		    if (currentFrame == null || currentFrame.IsFinished)
+		    {
+		        currentFrame = new Frame();
+                frames.Add(currentFrame);
+		    }
+
+            currentFrame.Roll(pins);
+		    score += pins;
+		    foreach (var frame in frames.Where(x => x.NeedBonuses > 0))
+		    {
+		        frame.AddBonus(pins);
+		        frame.NeedBonuses--;
+		    }
 		}
 
-		public int GetScore()
+	    private void CheckSpareOrStrike()
+	    {
+	        var previousFrame = frames.LastOrDefault(x => x.IsFinished);
+	        if (previousFrame?.IsSpare == true)
+	        {
+	            previousFrame.NeedBonuses += 1;	            
+	        }
+	        else if (previousFrame?.IsStrike == true)
+	        {
+                previousFrame.NeedBonuses += 2;
+	        }
+	    }
+
+	    public int GetScore()
 		{
-			return 0;
+			return frames.Sum(x => x.Score);
 		}
 	}
 
@@ -24,7 +81,145 @@ namespace Kontur.Courses.Testing.Tdd
 		[Test]
 		public void returnZero_beforeAnyRolls()
 		{
-			//TODO
+			var game = new Game();
+		    var score = game.GetScore();
+            Assert.AreEqual(0, score);
 		}
+
+
+        [Test]
+	    public void returnScore_afterFirstRoll()
+	    {
+	        var game = new Game();
+            game.Roll(5);
+            Assert.AreEqual(5, game.GetScore());
+	    }
+
+        [Test]
+	    public void returnCorrectScore_afterSpare()
+	    {
+	        var game = new Game();
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(1);
+            Assert.AreEqual(12, game.GetScore());
+	    }
+
+        [Test]
+	    public void returnCorrectScore_afterStrike()
+	    {
+	        var game = new Game();
+            game.Roll(10);
+            game.Roll(5);
+            game.Roll(1);
+            Assert.AreEqual(22, game.GetScore());
+	    }
+
+        [Test]
+	    public void returnCorrentScore_afterDoubleSpare()
+	    {
+	        var game = new Game();
+            game.Roll(1);
+            game.Roll(4);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(1);
+            Assert.AreEqual(41, game.GetScore());
+	    }
+
+        [Test]
+	    public void returnCorrectScore_afterDoubleSpareAndStrike()
+	    {
+	        var game = new Game();
+            game.Roll(1);
+            game.Roll(4);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(10);
+            game.Roll(0);
+            game.Roll(1);
+            Assert.AreEqual(61, game.GetScore());
+        }
+
+        [Test]
+	    public void returnCorrectScore_afterWholeGame()
+	    {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(4);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(10);
+            game.Roll(0);
+            game.Roll(1);
+            game.Roll(7);
+            game.Roll(3);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(10);
+            game.Roll(2);
+            game.Roll(8);
+            game.Roll(6);
+            Assert.AreEqual(133, game.GetScore());
+        }
+
+        [Test]
+	    public void returnCorrectScore_afterStrikeAndSpare()
+	    {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(4);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(10);
+            game.Roll(0);
+            game.Roll(1);
+            game.Roll(7);
+            game.Roll(3);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(8);
+            game.Roll(6);
+            Assert.AreEqual(141, game.GetScore());
+        }
+        [Test]
+	    public void returnCorrectScore_after12strikes()
+	    {
+	        var game = new Game();
+            for (int i = 0; i < 12; i++)
+            {
+                game.Roll(10);
+            }
+            Assert.AreEqual(300, game.GetScore());
+	    }
+        [Test]
+	    public void afterTwoStrikeAnd0()
+	    {
+	        var game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(0);
+            game.Roll(0);
+            Assert.AreEqual(60, game.GetScore());
+	    }
 	}
 }
